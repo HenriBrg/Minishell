ec91e21d637f7bff58b8fe7a2f1672996e89fc9c
Henri BERGER <hberger@e2r2p4.42.fr>
2020-03-10 18:32:58 +0100

    chevrons inside executable works

--- libft/src/others/get_next_line.c
+++ libft/src/others/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hberger <hberger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 15:08:42 by hberger           #+#    #+#             */
-/*   Updated: 2020/03/09 18:23:28 by hberger          ###   ########.fr       */
+/*   Updated: 2020/03/10 18:32:19 by hberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,36 @@
 ** if ((x = read(fd, buffer, BUFFER_SIZE)) < 0)
 ** return (0);
 */
+// 	if (read(0, buf, 1) == 0)
+// (ligne vide) ? exit_ctrl_d() : write(1, "  \b\b", 4);
+
+	//printf("STR = %s et BUFFER = %s et X = %d\n", str, buffer, x);
+
+		//printf("True\n");
+
+// else
+// 	printf("ELSE : STR = %s et BUFFER = %s et X = %d\n", str, buffer, x);
 
 static char	*ft_read(const int fd, char *str)
 {
+	int 	i;
 	int		x;
 	char	*tmp;
 	char	buffer[BUFFER_SIZE + 1];
 
-	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, buffer, 0) < 0)
+	if (!(i = 1) || fd < 0 || BUFFER_SIZE < 1 || read(fd, buffer, 0) < 0)
 		return (0);
 	str = (str == 0) ? ft_strnew(1) : str;
 	while (ft_strchr(str, '\n') == 0)
 	{
 		tmp = str;
-		if ((x = read(fd, buffer, BUFFER_SIZE)) <= 0)
-		{
+		if ((x = read(fd, buffer, BUFFER_SIZE)) <= 0 && (i++))
 			if (ft_strlen(str) && write(fd, "  \b\b", 4))
+			{
+				if (i > 500 && (str = ft_strjoin(str, "\n")))
+					return (str);
 				continue ;
-			else
-				write(fd, "  \b\b", 4);
-		}
+			}
 		buffer[x] = '\0';
 		str = ft_strjoin(str, buffer);
 		free(tmp);
