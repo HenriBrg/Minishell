3e73b0db44fb6fc762d22595cd487af0666ad25b
Henri BERGER <hberger@e2r2p4.42.fr>
2020-03-10 20:13:47 +0100

    path norm

--- srcs/executables/exec.c
+++ srcs/executables/exec.c
@@ -6,7 +6,7 @@
 /*   By: hberger <hberger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 18:29:47 by hberger           #+#    #+#             */
-/*   Updated: 2020/03/10 19:56:47 by hberger          ###   ########.fr       */
+/*   Updated: 2020/03/10 20:13:01 by hberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,15 +47,18 @@ char			*checkpath(char **cmds, struct stat *s, char *envpath)
 
 void			executablesnofork(char **cmds, t_envar *envar)
 {
-	int			ret;
 	char		*execpath;
 	struct stat	s;
 
-	execpath = 0;
-	if (ft_strstr(cmds[0], "./") && lstat(cmds[0], &s) == 0)
+	if (!(execpath = 0) && ft_strstr(cmds[0], "./") && lstat(cmds[0], &s) == 0)
 	{
 		if ((s.st_mode & S_IFREG) && (s.st_mode & S_IXUSR))
 			execpath = ft_strdup(cmds[0]);
+		else
+		{
+			ft_putstr_fd(cmds[0], 2);
+			return (ft_putendl_fd(": Permission denied", 2));
+		}
 	}
 	else if (ft_strstr(cmds[0], "/bin/") && lstat(cmds[0], &s) == 0)
 	{
@@ -64,11 +67,10 @@ void			executablesnofork(char **cmds, t_envar *envar)
 	}
 	else if ((execpath = checkpath(cmds, &s, getvar(envar, "PATH"))) == 0)
 	{
-			ft_putstr_fd("minishell: ", 2);
 			ft_putstr_fd(cmds[0], 2);
 			return (ft_putendl_fd(": command not found", 2));
 	}
-	ret = execve(execpath, cmds, g_environ_strstab);
+	g_exitvalue = execve(execpath, cmds, g_environ_strstab);
 	free(execpath);
-	exit((g_exitvalue = ret));
+	exit(g_exitvalue);
 }
