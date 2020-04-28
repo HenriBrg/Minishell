d3478a4ec78811ab2c70d7e4ea798f63cee2a876
HenriBrg <hnr.berger@gmail.com>
2020-04-28 20:44:43 +0200

    Fix ECHO

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,38 +6,11 @@
 /*   By: hberger <hberger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 18:11:47 by hberger           #+#    #+#             */
-/*   Updated: 2020/03/10 20:41:07 by hberger          ###   ########.fr       */
+/*   Updated: 2020/04/28 20:43:33 by henri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
-
-/*
-** echo() print chaque argument toujours séparés d'un espace
-** puis un \n sauf si flag -n
-** echo -n -n ..... n'affiche rien
-*/
-
-void		printecho(char *str)
-{
-	int		c;
-	char	*tmp;
-
-	c = 0;
-	tmp = ft_strtrim(str, " ");
-	while (tmp[c] != 0)
-	{
-		if (tmp[c] == ' ')
-		{
-			while (tmp[c] != 0 && tmp[c] == ' ')
-				c++;
-			write(1, " ", 1);
-		}
-		write(1, &(tmp[c]), 1);
-		c++;
-	}
-	free(tmp);
-}
 
 void		builtinecho(char **cmds, t_envar *envar)
 {
@@ -56,7 +29,7 @@ void		builtinecho(char **cmds, t_envar *envar)
 	}
 	while (cmds[i])
 	{
-		printecho(cmds[i]);
+		ft_putstr(cmds[i]);
 		if (cmds[i + 1])
 			write(1, " ", 1);
 		i++;
